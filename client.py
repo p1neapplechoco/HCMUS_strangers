700c6aa19804ba49410a34404fe5cc2dfaaf419b
sabertoaster <phidaotram123@gmail.com>
2024-10-01 14:25:02 +0700

    Code ngu

--- client.py
+++ client.py
@@ -1,24 +1,21 @@
 import socket
 
-HOST = "26.81.48.125"  # IP adress server
-PORT = 65432        # port is used by the server
+def start_client():
+    host = '26.81.48.125'  # The server's hostname or IP address
+    port = 65432        # The port used by the server
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = (HOST, PORT)
-print("Client connect to server with port: " + str(PORT))
-client.connect(server_address)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        print(f"Connected to server at {host}:{port}")
 
-try:
-    while True:
-        msg = input('Client: ')
-        if not msg:
-            continue
-        client.sendall(bytes(msg, "utf8"))
-        respond = client.recv(1024)
-        print(respond.decode("utf8"))
-    # client.sendall(b"This is the message from client")
-except KeyboardInterrupt:
-    client.close()
-finally:
-    client.close()
+        while True:
+            message = input("Enter a message to send (or 'quit' to exit): ")
+            if message.lower() == 'quit':
+                break
 
+            s.sendall(message.encode('utf-8'))
+            data = s.recv(1024)
+            print(f"Received from server: {data.decode('utf-8')}")
+
+if __name__ == "__main__":
+    start_client()
--- server.py
+++ server.py
@@ -1,25 +1,25 @@
-import socket 
+import socket
 
-HOST = "127.0.0.1"
-PORT = 65432
+def start_server():
+    host = '26.81.48.125'  # localhost
+    port = 65432        # port to listen on (non-privileged ports are > 1023)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-print("Waiting for Client")
-conn, addr = s.accept()
-try:
-    print("Connected by ", addr)
-    while True:
-        data = conn.recv(1024)
-        # if data == "":
-        #     print("Detect client close")
-        #     break
-        if len(data)> 0:
-            print("Server recv: " + data.decode("utf8"))
-except KeyboardInterrupt:
-    conn.close()
-    # s.close()
-finally:
-    conn.close()
-    # s.close()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen()
+        print(f"Server listening on {host}:{port}")
+
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                while True:
+                    data = conn.recv(1024)
+                    # if not data:
+                    #     break
+                    print(f"Received: {data.decode('utf-8')}")
+                    response = f"Server received: {data.decode('utf-8')}"
+                    conn.sendall(response.encode('utf-8'))
+
+if __name__ == "__main__":
+    start_server()
