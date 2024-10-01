8d5bcb6bdabb173578c69171f24ecc464440b76c
Nguyen Thien An <ngthienaans@gmail.com>
2024-10-01 15:07:35 +0700

    sigma

--- test.py
+++ test.py
@@ -1,42 +1,46 @@
 import subprocess
 import socket
 
-def get_ip_addresses():
-    # Run 'arp -a' command to get the list of connected devices and their IPs
-    result = subprocess.run(['arp', '-a'], capture_output=True, text=True)
-    output = result.stdout
-    ip_addresses = []
+class SIGMA:
+    def __init__(self):
+        self.ip = socket.gethostbyname(socket.gethostname())
+        self.ip_addresses = []
+        self.hosts = {}
+        pass
+
+    def get_connected_ip(self):
+        output = subprocess.run(['arp', '-a'], capture_output=True, text=True).stdout
+        output = output[output.find(self.ip):]
+
+        for line in output.splitlines():
+            if "dynamic" in line:
+                parts = line.split()
+                if len(parts) > 1:
+                    self.ip_addresses.append(parts[0].strip('()'))
+        
+        return self.ip_addresses
+        
+    def get_connected_hosts(self):
+        def get_host(ip):
+            try:
+                hostname, _, _ = socket.gethostbyaddr(ip)
+                return hostname
+            except socket.herror:
+                return None
+            
+        for ip in self.ip_addresses:
+            print(f"Getting hostname of {ip}")
+            hostname = get_host(ip)
+            if hostname:
+                self.hosts[ip] = hostname
+
+        return self.hosts
     
-    # Extract IP addresses from arp output
-    for line in output.splitlines():
-        if "dynamic" in line:
-            parts = line.split()
-            if len(parts) > 1:
-                ip_addresses.append(parts[0].strip('()'))  # Extract IP address
 
-    return ip_addresses
-
-def resolve_hostname(ip):
-    try:
-        # Use socket.gethostbyaddr to get hostname from IP
-        hostname, _, _ = socket.gethostbyaddr(ip)
-        return hostname
-    except socket.herror:
-        return None  # Couldn't resolve hostname
-
-def get_connected_hosts():
-    ip_addresses = get_ip_addresses()
-    hosts = {}
-
-    for ip in ip_addresses:
-        print(f"Getting hostname of {ip}")
-        hostname = resolve_hostname(ip)
-        hosts[ip] = hostname if hostname else "Unknown Host"
-
-    return hosts
-
-# Example usage
 if __name__ == "__main__":
-    hosts = get_connected_hosts()
-    for ip, hostname in hosts.items():
-        print(f"IP: {ip} -> Hostname: {hostname}")
+    this = SIGMA()
+    this.get_connected_ip()
+    this.get_connected_hosts()
+    
+    print(this.hosts)
+    
